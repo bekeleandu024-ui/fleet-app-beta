a3c93f7bc51692844cec91e330229717a0b74d1d
bekeleandu024-ui <bekeleandu024@gmail.com>
2025-11-09 07:11:25 -0500

    bye fleet ui

--- app/fleet/App.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { FleetManagement } from './components/FleetManagement';
-export function App() {
-  return <div className="w-full min-h-screen bg-gray-900">
-      <FleetManagement />
-    </div>;
-}
